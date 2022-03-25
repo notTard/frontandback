64c8b86c7ce21a502b2c445429f155673f810ad6
notTard <44521919+notTard@users.noreply.github.com>
2022-03-25 14:03:05 +0400

    Update EmployeeController.java

--- src/main/java/net/fbexample/demo/controller/EmployeeController.java
+++ src/main/java/net/fbexample/demo/controller/EmployeeController.java
@@ -3,9 +3,11 @@ package net.fbexample.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 
 import net.fbexample.demo.service.EmployeeService;
 
+@Controller
 public class EmployeeController {
 	
 	@Autowired
